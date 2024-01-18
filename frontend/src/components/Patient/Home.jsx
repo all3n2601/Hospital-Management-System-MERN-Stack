e10c4c1eacf458d6a51100727e73e6ddc7891239
hikigaya007 <bijop007@gmail.com>
2024-01-18 23:35:18 +0530

    service section for home page almost done

--- frontend/src/components/Patient/Home.jsx
+++ frontend/src/components/Patient/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../Shared/Navbar';
 import banner from "../../assets/hero.png"
+import service from "../../assets/services.png"
 function Home() {
   return (
     <div className='bg-slate-300'>
@@ -14,11 +15,20 @@ function Home() {
                         services of world standard and to provide them to all sections....
                     </p>
                 </div>
-                <div className='w-full md:w-[80%] lg:w-[80%]'><img src={banner} alt="hero"  className='h-[400px]'/></div>
+                <div className='w-full md:w-[80%] lg:w-[80%]'><img src={banner} alt="hero"  className='h-[400px]  shadow-black'/></div>
             </div>
         
         </section>
-        <section>Our services</section>
+        <section>
+            <div className='w-full h-full flex flex-col justify-center items-center'>
+                <p className='font-semibold text-3xl'>Why Choose Us?</p>
+                <div className='flex flex-col md:flex-row justify-center  items-center'>
+                    <div>first</div>
+                    <div className='hidden md:block'><img src={service} alt="services" className='h-[400px]' /></div>
+                    <div>third</div>
+                </div>
+            </div>
+        </section>
         <section>our docs</section>
         <section>People thought</section>
         <section>footer</section>
