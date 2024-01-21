4992b1a7198b3695acac01a784ebf2fe7f27348e
hikigaya007 <bijop007@gmail.com>
2024-01-21 13:54:33 +0530

    small typo error changed

--- frontend/src/components/Admin/AdminDoctor.jsx
+++ frontend/src/components/Admin/AdminDoctor.jsx
@@ -53,7 +53,7 @@ function AdminDoctor() {
                                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
                                         <th scope="col" className="px-6 py-3">
-                                            Docotor Name
+                                            Doctor Name
                                         </th>
                                         <th scope="col" className="px-6 py-3">
                                             Doctor Id
--- frontend/src/components/Admin/AdminPatient.jsx
+++ frontend/src/components/Admin/AdminPatient.jsx
@@ -44,10 +44,10 @@ function AdminPatient() {
                                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
                                         <th scope="col" className="px-6 py-3">
-                                            Docotor Name
+                                            Patient Name
                                         </th>
                                         <th scope="col" className="px-6 py-3">
-                                            Doctor Id
+                                        Patient Id
                                         </th>
                                         <th scope="col" className="px-6 py-3">
                                             Department
