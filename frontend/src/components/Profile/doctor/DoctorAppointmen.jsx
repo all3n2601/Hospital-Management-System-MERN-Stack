7f2482049451a2862f624336157d1d45dfc76550
Allen Febi M A <allenabi2614@gmail.com>
2024-03-04 00:53:12 +0530

    Refactor DoctorAppointmen component

--- frontend/src/components/Profile/doctor/DoctorAppointmen.jsx
+++ frontend/src/components/Profile/doctor/DoctorAppointmen.jsx
@@ -4,34 +4,27 @@ import profiePic from "../../../assets/doct2.jpg";
 import axios from "axios";
 import Swal from "sweetalert2";
 import DoctorSidebar from "./DoctorSidebar";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 function DoctorAppointmen() {
   const [appointments, setAppointments] = useState([]);
 
-  const {currentUser} = useSelector((state) => state.user)
-
-
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://hmsmern.onrender.com/appointment/get-appointment/${currentUser._id}`);
-       setAppointments(response.data);
-        
+        const response = await axios.get(
+          `https://hmsmern.onrender.com/appointment/get-appointment/${currentUser._id}`
+        );
+        setAppointments(response.data);
       } catch (error) {
-        console.error('Error fetching users:', error);
-
+        console.error("Error fetching users:", error);
       }
     };
-  
+
     fetchData();
-  
-  }, []); 
-
-    
-
-   
+  }, []);
 
   return (
     <section className="bg-slate-300 flex justify-center items-center">
@@ -59,15 +52,16 @@ function DoctorAppointmen() {
                     <th scope="col" className="px-6 py-3">
                       Status
                     </th>
-                    
                   </tr>
                 </thead>
                 <tbody>
-                  {appointments ?
+                  {appointments &&
+                  Array.isArray(appointments) &&
+                  appointments.length > 0 ? (
                     appointments.map((item, index) => (
                       <tr key={item._id} className="text-black">
                         <td scope="col" className="px-6 py-3">
-                          {index+1}
+                          {index + 1}
                         </td>
                         <td scope="col" className="px-6 py-3">
                           {item.patient}
@@ -80,14 +74,20 @@ function DoctorAppointmen() {
                         </td>
                         <td scope="col" className="px-6 py-3">
                           {item.status}
-                        </td>    
+                        </td>
                       </tr>
-                    )):<p>Sorry You have No appointments !!</p>}
+                    ))
+                  ) : (
+                    <tr>
+                      <td className="px-6 py-3" colSpan="5">
+                        <p>Sorry, You have No appointments !!</p>
+                      </td>
+                    </tr>
+                  )}
                 </tbody>
               </table>
             </div>
           </div>
-          
         </div>
       </div>
     </section>
